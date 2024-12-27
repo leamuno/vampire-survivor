8ab47e79dc460a702ceb1e48a9ae3674359f2c32
Leandro Munoz <Leandro.MunozJr@gmail.com>
2024-12-27 20:16:15 +0900

    Adds movement to the player character

---
+++ Player/Player.cs
@@ -0,0 +1,25 @@
+using Godot;
+
+public partial class Player : CharacterBody2D
+{
+    public float movementSpeed = 40f;
+
+    public override void _PhysicsProcess(double delta)
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        // Get input
+        float xMov = Input.GetActionStrength("right") - Input.GetActionStrength("left");
+        float yMov = Input.GetActionStrength("down") - Input.GetActionStrength("up");
+
+        // Calculate movement vector
+        Vector2 mov = new Vector2(xMov, yMov);
+        Velocity = mov.Normalized() * movementSpeed;
+
+        // Move and slide
+        MoveAndSlide();
+    }
+}

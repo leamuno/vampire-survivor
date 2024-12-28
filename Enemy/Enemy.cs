045f9e036e7b7ef4b39830604064a46ffd715c41
Leandro Munoz <Leandro.MunozJr@gmail.com>
2024-12-28 20:20:00 +0900

    Adds some animations to the models

--- Enemy/Enemy.cs
+++ Enemy/Enemy.cs
@@ -6,10 +6,14 @@ public partial class Enemy : CharacterBody2D
   public float movementSpeed = 20f;
 
   private Node2D player;
-
+  private Sprite2D sprite; // Declare the Sprite2D variable
+  private AnimationPlayer anim;
   public override void _Ready()
   {
     player = (Node2D)GetTree().GetFirstNodeInGroup("player");
+    sprite = GetNode<Sprite2D>("Sprite2D");
+    anim = GetNode<AnimationPlayer>("AnimationPlayer");
+    anim.Play("walk");
   }
 
   public override void _PhysicsProcess(double delta)
@@ -18,5 +22,14 @@ public partial class Enemy : CharacterBody2D
     Vector2 direction = GlobalPosition.DirectionTo(player.GlobalPosition);
     Velocity = direction * movementSpeed;
     MoveAndSlide();
+
+    if (direction.X > 0.1)
+    {
+      sprite.FlipH = true; // Flip horizontally
+    }
+    else if (direction.X < -0.1)
+    {
+      sprite.FlipH = false; // Reset flip
+    }
   }
 }
--- Player/Player.cs
+++ Player/Player.cs
@@ -4,6 +4,15 @@ public partial class Player : CharacterBody2D
 {
   private float movementSpeed = 40f;
 
+  private Sprite2D sprite; // Declare the Sprite2D variable
+  private Timer walkTimer;
+
+  public override void _Ready()
+  {
+    // Get the Sprite2D node
+    sprite = GetNode<Sprite2D>("Sprite2D");
+    walkTimer = GetNode<Timer>("WalkTimer");
+  }
   public override void _PhysicsProcess(double delta)
   {
     Movement();
@@ -17,8 +26,32 @@ public partial class Player : CharacterBody2D
 
     // Calculate movement vector
     Vector2 mov = new Vector2(xMov, yMov);
-    Velocity = mov.Normalized() * movementSpeed;
+    if (mov.X > 0)
+    {
+      sprite.FlipH = true; // Flip horizontally
+    }
+    else if (mov.X < 0)
+    {
+      sprite.FlipH = false; // Reset flip
+    }
 
+    if (mov != Vector2.Zero)
+    {
+      if (walkTimer.IsStopped())
+      {
+        if (sprite.Frame >= sprite.Hframes - 1)
+        {
+          sprite.Frame = 0;
+        }
+        else
+        {
+          sprite.Frame += 1;
+        }
+        walkTimer.Start();
+      }
+    }
+
+    Velocity = mov.Normalized() * movementSpeed;
     // Move and slide
     MoveAndSlide();
   }
